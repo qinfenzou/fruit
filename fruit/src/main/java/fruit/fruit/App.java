b64021e08c225ce002b0dbff395304b6b227aa85
unknown <qinfenzou@gmail.com>
2017-11-20 16:31:15 +0800

    初始web目录结构

---
+++ fruit/src/main/java/fruit/fruit/App.java
@@ -0,0 +1,13 @@
+package fruit.fruit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
